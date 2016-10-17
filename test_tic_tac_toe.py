5c5eeaa434c2ea63ab89b1db6e2d5a4cf04e2710
Marcin <marcin-pieczynski@wp.pl>
2016-10-17 23:06:27 +0200

    first commit of tic-tac-toe

---
+++ test_tic_tac_toe.py
@@ -0,0 +1,68 @@
+
+import unittest
+from tic_tac_toe import tic_tac_toe_state_checker
+
+
+class Tic_Tac_Toe_State_Checker_Test(unittest.TestCase):
+    """Tests for tic_tac_toe_state_checker from tic_tac_toe.py.
+    Checks weather:
+    - board is NxN
+    - who won X, O or none
+    """
+
+    def test_tic_tac_toe_1(self):
+        """Test - none won board."""
+        board_1 = [
+            "XO.",
+            ".OX",
+            ".X."
+            ]
+        self.assertEqual(tic_tac_toe_state_checker(board=board_1), ".")
+
+    def test_tic_tac_toe_2(self):
+        """Test - X won board.
+        Board contains extra empty line at beginning and end."""
+        board_2 = [
+            "X...",
+            ".X..",
+            "..X.",
+            "...X",
+            ]
+        self.assertEqual(tic_tac_toe_state_checker(board=board_2), "X")
+
+    def test_tic_tac_toe_3(self):
+        """Test - none won board."""
+        board_3 = [
+            "X...",
+            ".X..",
+            "..X.",
+            "....",
+            ]
+        self.assertEqual(tic_tac_toe_state_checker(board=board_3), ".")
+
+    def test_tic_tac_toe_4(self):
+        """Test - O won board."""
+        board_4 = [
+            "..O..",
+            "..O..",
+            "..O..",
+            "..O..",
+            "..O..",
+            ]
+        self.assertEqual(tic_tac_toe_state_checker(board=board_4), "O")
+
+    def test_tic_tac_toe_5(self):
+        """Test - board is not NxN, return False."""
+
+        board_5 = [
+            "XXOO.",
+            "...X.",
+            "OOOO",
+            "...",
+            "....."
+            ]
+        self.assertEqual(tic_tac_toe_state_checker(board=board_5), False)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tic_tac_toe.py
@@ -0,0 +1,26 @@
+
+
+
+def tic_tac_toe_state_checker(board):
+    """
+    Funkcja sprawdzająca stan gry w 'Kółko i krzyżyk'.
+    Jako arg funkcja przyjmuje planszę w postali przykładowej listy:
+    board = [
+        "XO.",
+        ".0X",
+        ".X."
+    ]
+    Dla planszy 3x3 funkcja zwraca:
+    "X" jeśli X wygrywa.
+    "Y" jeśli Y wygrywa.
+    "." jeśli nikt nie wygrywa.
+
+    #########
+    Advanced  PART
+    ########
+    Funkcja działa dla dowolnej wielkości planszy kwadratowej NxN.
+    Funkcja zwraca False jeśli plansza nie jest NxN (nie jest kwadratowa)
+    """
+    pass
+
+
